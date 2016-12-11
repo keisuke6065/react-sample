baa1ea0020ae00e56d44269e1f047334dfe5f156
keisuke6065 <keisuke6065@gmail.com>
2016-12-12 00:53:33 +0900

    Material Design Lite

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React, {Component} from 'react'; 
+import React, { Component} from 'react';
+import {Link} from 'react-router';
 import Nav from 'components/template/Nav.js';
 
+
 class App extends Component {
+  componentDidMount() {
+    if (typeof componentHandler !== 'undefined') { // eslint-disable-line
+      componentHandler.upgradeDom(); // eslint-disable-line
+    }
+  }
+
+  componentDidUpdate() {
+    if (typeof componentHandler !== 'undefined') { // eslint-disable-line
+      componentHandler.upgradeDom(); // eslint-disable-line
+    }
+  }
+
   render() {
-    // ナビゲーションを作る
     return (
-      <div className="App">
-        <Nav></Nav>
-        <div className="">
-          {this.props.children}
+        <div className="mdl-layout mdl-js-layout">
+          <header className="mdl-layout__header">
+            <div className="mdl-layout__header-row">
+              <div className="mdl-layout-spacer"></div>
+              <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+                <label className="mdl-button mdl-js-button mdl-button--icon" htmlFor="fixed-header-drawer-exp">
+                  <i className="material-icons">search</i>
+                </label>
+                <div className="mdl-textfield__expandable-holder">
+                  <input className="mdl-textfield__input" type="text" name="sample" id="fixed-header-drawer-exp"/>
+                </div>
+              </div>
+              </div>
+          </header>
+          
+            <div className="mdl-layout__drawer">
+              <span className="mdl-layout-title">Title</span>
+              <nav className="mdl-navigation">
+                <Link to="/todo" className="mdl-navigation__link">todo</Link>
+                <a className="mdl-navigation__link" href="">Link</a>
+                <a className="mdl-navigation__link" href="">Link</a>
+              </nav>
+            </div>
+            <main className="mdl-layout__content">
+              <div className="page-content">{this.props.children}</div>
+            </main>
         </div>
-      </div>
-    );
+        );
   }
 }
 
--- src/components/todo.js
+++ src/components/todo.js
@@ -1,11 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class TodoApp extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {items: [], text: ''};
+    this.state = { items: [], text: '' };
+  }
+
+  componentDidMount() {
+    if (typeof componentHandler !== 'undefined') { // eslint-disable-line
+      componentHandler.upgradeDom(); // eslint-disable-line
+    }
+  }
+
+  componentDidUpdate() {
+    if (typeof componentHandler !== 'undefined') { // eslint-disable-line
+      componentHandler.upgradeDom(); // eslint-disable-line
+    }
   }
 
   render() {
@@ -13,16 +25,22 @@ class TodoApp extends Component {
       <div>
         <h3>TODO</h3>
         <TodoList items={this.state.items} />
+
         <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} value={this.state.text} />
-          <button>{'Add #' + (this.state.items.length + 1)}</button>
+          <div className="mdl-textfield mdl-js-textfield">
+            <input onChange={this.handleChange} value={this.state.text} className="mdl-textfield__input" type="text" id="sample1" />
+            <label className="mdl-textfield__label" htmlFor="sample1">Text...</label>
+          </div>
+          <br />
+          <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">{'Add #' + (this.state.items.length + 1)}</button>
         </form>
+
       </div>
     );
   }
 
   handleChange(e) {
-    this.setState({text: e.target.value});
+    this.setState({ text: e.target.value });
   }
 
   handleSubmit(e) {
@@ -43,9 +61,19 @@ class TodoList extends Component {
     return (
       <ul>
         {this.props.items.map(item => (
-          <li key={item.id}>{item.text}</li>
+          <li className="mdl-list__item" key={item.id}>
+            <span className="mdl-list__item-primary-content">
+              <i className="material-icons mdl-list__item-icon">person</i>
+              {item.text}
+            </span>
+          </li>
         ))}
       </ul>
+
+
+
+
+
     );
   }
 }
