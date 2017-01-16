19ee602121e0c2c0cf04b65217858b661cb97936
中村 恵介 <keisuke.nakamura@bizreach.co.jp>
2017-01-16 20:33:15 +0900

    loop bug

--- src/components/2048/2048.js
+++ src/components/2048/2048.js
@@ -4,7 +4,7 @@ class game extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            board:[[2,0,2,2],
+            board:[[2,2,2,2],
             [0,0,0,0],
             [0,0,0,0],
             [0,0,0,0]]
@@ -16,7 +16,6 @@ class game extends Component {
     keyHandler(e){
         console.log(e.keyCode)
         var xCount = 0
-        var yCount = 0
         var newBoard = this.state.board
         var random = Math.floor(Math.random() * 2, 0) ? 2 : 4
         var frist = Math.floor(Math.random()*4)
@@ -28,6 +27,7 @@ class game extends Component {
         // 普通のforに書き換える
         this.state.board.forEach(function(x) {
             console.log(x)
+            var yCount = 0
             x.forEach(function(y) {
                 // left
                 if(e.keyCode === 37){
@@ -35,11 +35,11 @@ class game extends Component {
                     if(newBoard[xCount][yCount+1] === y
                         && y !== 0)
                         newBoard[xCount][yCount] = y*2
+                        console.log('test' + x + 'test' )
                     if(newBoard[xCount][yCount-1] === y*2
                         && y !== 0){
                         newBoard[xCount][yCount] = 0
                     }
-                    console.log(newBoard)
                 }
                 // right
                 // down
@@ -53,6 +53,7 @@ class game extends Component {
                     if( a < b  && a === 0) return 1;
                     return 0;
                 })
+                console.log('sort' + newBoard[xCount])
             }
             xCount++
         }, this);
